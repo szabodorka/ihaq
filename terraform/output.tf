a151df983b1d4a93de9463a15a42ff78cb43d387
szabodorka <szabodorkam@gmail.com>
2025-09-10 13:45:25 +0200

    Moved LBC module to EKS & cleanup providers.tf (previously terraform.tf)

--- terraform/output.tf
+++ terraform/output.tf
@@ -9,7 +9,3 @@ output "rds_endpoint" {
 output "kubeconfig_command" {
     value = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region eu-central-1"
 }
-
-output "openid_connect_provider_arn" {
-  value = aws_iam_openid_connect_provider.eks.arn
-}
