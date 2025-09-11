c4ec5218884969f313df566f36062bc83d4f071d
szabodorka <szabodorkam@gmail.com>
2025-09-11 19:30:57 +0200

    Create separate versions.tf, correct providers.tf, cleanup main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "eu-central-1"
-}
-
 module "vpc" {
     source = "./modules/vpc"
 }
@@ -11,6 +7,9 @@ module "eks" {
   vpc_id = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnet_ids
   public_subnet_ids = module.vpc.public_subnet_ids
+  cluster_oidc_issuer = module.eks.cluster_oidc_issuer
+  cluster_name = module.eks.cluster_name
+  openid_connect_provider = module.eks.openid_connect_provider_arn
 }
 
 module "rds" {
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,10 +1,3 @@
-terraform {
-  required_version = ">= 1.3.2"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.95"
-    }
-  }
+provider "aws" {
+  region = "eu-central-1"
 }
